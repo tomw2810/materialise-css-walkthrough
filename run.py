c35430efeff28b87764feac9ed46b78282de719b
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-06-26 19:03:05 +0000

    file updated

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-import os
-from taskmanager import app
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(
